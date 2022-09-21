9e03e46df1b702877891ed4b694b33fee8deb687
Fernando <flgorordo@gmail.com>
2022-09-21 14:57:49 -0300

    minor fixes

--- root/frontend/src/components/Header/Header.jsx
+++ root/frontend/src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PrimaryButton from '../common/Buttons/PrimaryButton/PrimaryButton';
 
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <Box>
+      <Container sx={{display:"flex",justifyContent:"space-between"}}>
+        <Box sx={{height:"90px",display:"flex",alignItems:"center"}}>
+          <Typography variant="h3" component={Link} to="/" sx={{textDecoration:"none", color:"#000"}}>
+            myWallet
+          </Typography>
+        </Box>
+        <Box sx={{display:{xs:"none",md:"block"}}}>
+          Desktop Menu
+        </Box>
+        <Box sx={{display:{xs:"block",md:"none"}}}>
+          Mobile Menu
+        </Box>
+
+      </Container>
+    </Box>
   )
 };
 
--- root/frontend/src/main.jsx
+++ root/frontend/src/main.jsx
@@ -2,14 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
+const theme = createTheme({
+  typography:{
+    fontFamily: "Nunito, sans-serif",
+    fontWeightBold:700,
+    fontWeightMedium:500,
+    fontWeightRegular:400,
+    fontWeightLight:300,
+  },
+  pellete:{
+
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
